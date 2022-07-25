a3dd78873b9c631e874877166e2cd3204cf932ac
matheuslima23 <ml3666763@gmail.com>
2022-07-25 10:51:55 -0300

    mudanças de paleta de cores,e algumas pequenas modificações

--- index.js
+++ index.js
@@ -38,15 +38,73 @@ function GeraJogoMega(){
       let josgosMega=[]
     for (let i=0;i<njogos;i++){
         josgosMega.push("jogo"+i)
+
+        
     }
     document.getElementById("c3-resultado").innerHTML=josgosMega
 
-}
-
-
-function CriarJogo(){
+    let sorteados=[];
+    function sorteio (){
+      return Math.floor(Math.random()*60);
+    }
     
+    while(sorteados.length<6){
+      let resultado=sorteio();
+       if(sorteados.indexOf(resultado )===-1){
+         sorteados.push(resultado )
+       }
+      }
+
+
+
+
+
+
+
+
+
+
 }
+
+
+//function CriarJogo(){
+    
+function soma(){
+    let n1=parseFloat(document.getElementById("n1").value);
+   let n2=parseFloat(document.getElementById("n2").value);
+    let resultado=n1+n2
+    document.getElementById("c5-resultado").innerHTML=resultado
+
+}
+function subtração(){
+    let n1=parseFloat(document.getElementById("n1").value);
+    let n2=parseFloat(document.getElementById("n2").value);
+    let resultado=n1-n2
+    document.getElementById("c5-resultado").innerHTML=resultado
+}
+function multipliucação(){
+    let n1=parseFloat(document.getElementById("n1").value);
+    let n2=parseFloat(document.getElementById("n2").value);
+    let resultado=n1*n2
+    document.getElementById("c5-resultado").innerHTML=resultado
+}
+function divisão(){
+    let n1=parseFloat(document.getElementById("n1").value);
+    let n2=parseFloat(document.getElementById("n2").value);
+    let resultado=n1/n2
+    document.getElementById("c5-resultado").innerHTML=resultado
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
